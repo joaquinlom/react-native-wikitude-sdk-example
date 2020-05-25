36e9a7fee15113e4bb81c575f41939f63f8e92ad
joaquin <joaquin.lom@spinar.mx>
2020-05-25 10:33:16 -0600

    update gradle

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React,{useEffect,useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -28,18 +28,17 @@ import {
 
 import {WikitudeView} from 'react-native-wikitude-sdk';
 const config = {
-  licenseKey: 'SM77tH5TA9wTlRnJfaovgS3hDg0OjDbL9cn6A3Nv5Qbozpn+tuhDkBA6sVLPCj1YXt/u+A50ZAO4oIpORPcuZq5sivcE9TpEKibNvTs2O8y2POZXCCOLeoH9IXwOtbHgrdQxv8KIp4y0nWwzMX5J00pRUzbwUSvWoYg0GutYAKdTYWx0ZWRfX0pInanyhBWDZ6pUxiswXqcwN5/QdrrhDPI1MnmzNckZAU2zfgVEiTwmMgiJfOr6IccFYtkR5UnCk+4kpdIeCbupNmzMPjKNJdEMZ2woFQDuyKjchhMna4oSR8jktMlTyJbWYxKUZDiYiHTC6uYcpJjHG++PIv11uL6rVAZqBTqGYVnhKbdrY8RpxBIJh9wS4pWMo8mFDwO6Y58U8JOlm0G7BnsZtFN6fPoo+KYDW02J+QjLKrcHsYiloeShrjJQHGvCkknSNcBSkq7Yj87fDWCIqBf76cQCjq0kV4RZhKss8FC6d8DalZuahTm0mzWXMICJSrX8aweonAAWX5NJT6F8AyHicaXpZpM47WXJULkrChkGQ5AXUelhzwFKS3sivZIBegIcfKx8Ab9PFbhpbL0uVJ/rfc2nc2ryzkHLPYTx2wI0jXdgijvFiMyyJgVGV3LO+9wCrYv6o2zfRD68uhRBMDFNNUgrNiEF1Tmn/Pf4J3llher1bbI6QE/p+OWhW2jZWGBXeqKk0k5GLkJJpzIFklu/sODNBjfJt1K0BJkRuR1iQaA5hsstQakTUIBJ6Peuji4qWWXdPoczipGNglTOal3eqiac5qf54V1GO2SDoq+9gjpXYgEDVZOQ7gNLtwuheVHzlKoLCrUUxWBheZS2Gjn3MrAfrljqq/13MNcE3xysll6pob8=',
+  licenseKey:
+    'SM77tH5TA9wTlRnJfaovgS3hDg0OjDbL9cn6A3Nv5Qbozpn+tuhDkBA6sVLPCj1YXt/u+A50ZAO4oIpORPcuZq5sivcE9TpEKibNvTs2O8y2POZXCCOLeoH9IXwOtbHgrdQxv8KIp4y0nWwzMX5J00pRUzbwUSvWoYg0GutYAKdTYWx0ZWRfX0pInanyhBWDZ6pUxiswXqcwN5/QdrrhDPI1MnmzNckZAU2zfgVEiTwmMgiJfOr6IccFYtkR5UnCk+4kpdIeCbupNmzMPjKNJdEMZ2woFQDuyKjchhMna4oSR8jktMlTyJbWYxKUZDiYiHTC6uYcpJjHG++PIv11uL6rVAZqBTqGYVnhKbdrY8RpxBIJh9wS4pWMo8mFDwO6Y58U8JOlm0G7BnsZtFN6fPoo+KYDW02J+QjLKrcHsYiloeShrjJQHGvCkknSNcBSkq7Yj87fDWCIqBf76cQCjq0kV4RZhKss8FC6d8DalZuahTm0mzWXMICJSrX8aweonAAWX5NJT6F8AyHicaXpZpM47WXJULkrChkGQ5AXUelhzwFKS3sivZIBegIcfKx8Ab9PFbhpbL0uVJ/rfc2nc2ryzkHLPYTx2wI0jXdgijvFiMyyJgVGV3LO+9wCrYv6o2zfRD68uhRBMDFNNUgrNiEF1Tmn/Pf4J3llher1bbI6QE/p+OWhW2jZWGBXeqKk0k5GLkJJpzIFklu/sODNBjfJt1K0BJkRuR1iQaA5hsstQakTUIBJ6Peuji4qWWXdPoczipGNglTOal3eqiac5qf54V1GO2SDoq+9gjpXYgEDVZOQ7gNLtwuheVHzlKoLCrUUxWBheZS2Gjn3MrAfrljqq/13MNcE3xysll6pob8=',
   url: 'http://spinar-dev.com/wikitude/We3/principal/index.html',
-}
+};
 
-
-const App = ()=>{
-
-  const wikitudeView = useRef(null)
+const App = () => {
+  const wikitudeView = useRef(null);
 
   useEffect(() => {
     //wikitudeView.resumeRendering();
-  }, [] )
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -54,10 +53,15 @@ const App = ()=>{
         />
       </View>
       <Separator />
-      <WikitudeView ref={wikitudeView} licenseKey={config.licenseKey} url={config.url} style={styles.AR} />
+      <WikitudeView
+        ref={wikitudeView}
+        licenseKey={config.licenseKey}
+        url={config.url}
+        style={styles.AR}
+      />
     </SafeAreaView>
-  )
-}
+  );
+};
 function Separator() {
   return <View style={styles.separator} />;
 }
